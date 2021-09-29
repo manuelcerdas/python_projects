9634586e941d609cff93efd37c67e93ab770a482
Manuel Cerdas <m.cerdas.salazar@accenture.com>
2021-09-29 16:32:29 -0600

    Plotlib map now working

--- Map/map.py
+++ Map/map.py
@@ -1,6 +1,30 @@
-import geopandas as gpd
-gpd.datasets.available
+import json
+import pandas as pd
+import plotly.express as px
 
-data = gpd.read_file("Cantonal.geojson")
 
-print (data.head)
+#load geographic data
+
+with open ("Cantonal.geojson") as f:
+    mapa = json.load (f)
+
+
+#load the birth data
+datos = pd.read_csv("nacimientos.csv")    
+
+print ()
+
+
+fig = px.choropleth(
+    datos, 
+    geojson=mapa, 
+    locations='cod_canton',
+    featureidkey="properties.cod_canton",
+    color='nacimientos',
+    color_continuous_scale="Viridis",
+    range_color=(datos["nacimientos"].min(), datos["nacimientos"].max()),
+    scope="north america",
+    labels={'nacimientos':'Cantidad de nacimientos'}
+)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
